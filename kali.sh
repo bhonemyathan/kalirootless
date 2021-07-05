78609be3361e8549d948ce8674b2797953745ff5
noobprogrammerD4V8ll <83602194+noobprogrammerD4V8ll@users.noreply.github.com>
2021-07-05 09:07:45 +0000

    Add files via upload

---
+++ kali.sh
@@ -0,0 +1,24 @@
+echo -e '\e[1;31m Please Wait.....\e[0m'
+pkg install figlet >/dev/null 2>&1
+clear
+figlet Kali Rootless
+echo ""
+echo -e '\e[1;30m Requirements:Ram 4gb,storage 64gb,Data Package 5gb,Install Nethunter-KeX client(url-store.nethunter.com) \e[0m'
+echo ""
+echo '[1]Start'
+echo ""
+echo '[2]Exit'
+echo ""
+read -p "Enter Number:" option
+case $option in 
+1)
+termux-setup-storage
+pkg install wget -y
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux ;;
+2)
+exit ;;
+*)
+echo -e 'e\[1;31m Thank for using my tool e\[0m';;
+esac
